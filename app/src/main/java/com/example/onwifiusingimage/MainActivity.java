df2058d8e9fa27e2ae4674733ff0499b63cdcb69
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-03 20:23:16 +0530

    take reference variables.

--- app/src/main/java/com/example/onwifiusingimage/MainActivity.java
+++ app/src/main/java/com/example/onwifiusingimage/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.onwifiusingimage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean wifi = false;
+    ImageButton i;
+    WifiManager wf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
